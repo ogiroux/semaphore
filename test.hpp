67baf1a60c68580158451af1a53c282760ce00ef
Olivier Giroux <ogiroux@gmail.com>
2017-07-27 16:18:01 -0700

    Different design for the CUDA lock.

--- test.hpp
+++ test.hpp
@@ -262,66 +262,61 @@ namespace details {
 struct mutex
 {
     __test_abi inline void unlock() noexcept {
-        tocket.fetch_add(1, std::memory_order_relaxed);
-        atom.fetch_and(~__valubit, std::memory_order_release);
+        __tocket.fetch_add(1, std::memory_order_relaxed);
+        __atom.fetch_and(~__valubit, std::memory_order_release);
     }
 
-    __test_abi inline void lock(std::memory_order order = std::memory_order_seq_cst) noexcept {
+    __test_abi inline void lock() noexcept {
+        uint32_t const tick = __ticket.fetch_add(1, std::memory_order_relaxed);
         uint32_t old = 0;
         for (int i = 0; i < 64; ++i) {
-            if(atom.compare_exchange_weak(old = 0, __valubit, order, std::memory_order_relaxed)) {
-                ticket.fetch_add(1, std::memory_order_relaxed);
+            if(__atom.compare_exchange_weak(old = 0, __valubit, std::memory_order_acquire, std::memory_order_relaxed))
                 return;
-            }
-            for (; old != 0 && i < 64; ++i, old = atom.load(std::memory_order_relaxed))
+            for (; old != 0 && i < 64; ++i, old = __atom.load(std::memory_order_relaxed))
                 details::__yield();
         }
-        __lock_slow(order);
+        __lock_slow(tick);
     }
 
-    __test_abi constexpr mutex() noexcept : atom(0), ticket(0), tocket(5) {
+    static constexpr uint32_t __valubit = 1;
+    static constexpr uint32_t __contbit = 2;
+
+    __test_abi constexpr mutex() noexcept : __atom(0), __ticket(0), __tocket(0) {
     }
 
     mutex(const mutex&) = delete;
     mutex &operator=(const mutex&) = delete;
 
-private:
-    static constexpr uint32_t __valubit = 1;
-    static constexpr uint32_t __contbit = 2;
-
-    __test_abi void __lock_slow(std::memory_order order) noexcept
+    __test_abi void __lock_slow(uint32_t const tick) noexcept
     {
-        details::__exponential_backoff b;
-        auto old = atom.fetch_add(__contbit, std::memory_order_acquire);
-        auto const tick = ticket.fetch_add(1, std::memory_order_relaxed);
-        auto tock = tocket.load(std::memory_order_relaxed);
-        auto const maxdiff = (std::numeric_limits<uint32_t>::max)() >> 1;
-        auto ready = (tock >= tick || tick - tock > maxdiff);
-        for (int i = 0; ; ++i) {
-            if(i < 64)
-                details::__yield();
-            else 
-                b.sleep();
-            if(!ready) {
-                tock = tocket.load(std::memory_order_relaxed);
-                ready = (tock >= tick || tick - tock > maxdiff);
-                if(ready)
-                    b.reset();
-                else
-                    continue;
+        uint32_t contbit = 0;
+        for(int i = 0;;++i) {
+            uint32_t old = __atom.load(std::memory_order_relaxed);
+            uint32_t const tock = __tocket.load(std::memory_order_relaxed);
+            uint32_t const pos_balance = tock > tick ? tock - tick : 0;
+            uint32_t const neg_balance = tock < tick ? tick - tock : 0;
+            if(tock == tick || pos_balance) {
+                if(pos_balance > 32 && !contbit)
+                    old = __atom.fetch_add(contbit = __contbit, std::memory_order_relaxed) + __contbit;
+                while ((old & __valubit) == 0) {
+                    if (contbit == 0 && (old & ~__valubit))
+                        break;
+                    uint32_t const next = old - contbit + __valubit;
+                    if (__atom.compare_exchange_weak(old, next, std::memory_order_acquire, std::memory_order_relaxed))
+                        return;
+                }
             }
-            old = atom.load(std::memory_order_relaxed);
-            while ((old & __valubit) == 0) {
-                auto next = old - __contbit + __valubit;
-                if (atom.compare_exchange_weak(old, next, order, std::memory_order_relaxed))
-                    return;
-            }
+#if !defined(__CUDA_ARCH__)
+            std::this_thread::sleep_for(std::chrono::nanoseconds(neg_balance * 128 + 64));
+#elif defined(__has_cuda_nanosleep)
+            details::__mme_nanosleep(neg_balance * 128 + 64);
+#endif
         }
     }
 
-    mutable atomic<uint32_t> atom;
-    mutable atomic<uint32_t> ticket;
-    mutable atomic<uint32_t> tocket;
+    atomic<uint32_t> __atom;
+    atomic<uint32_t> __ticket;
+    atomic<uint32_t> __tocket;
 };
 
 }}}
@@ -329,3 +324,84 @@ private:
 #endif //__NVCC__
 
 #endif //TEST_HPP
+
+
+/*
+
+
+struct mutex
+{
+    __test_abi inline void unlock() noexcept {
+        __tocket.fetch_add(1, std::memory_order_relaxed);
+        __atom.fetch_and(~__valubit, std::memory_order_release);
+    }
+
+    __test_abi inline void lock(std::memory_order order = std::memory_order_seq_cst) noexcept {
+        uint32_t old = 0;
+        for (int i = 0; i < 64; ++i) {
+            if(__atom.compare_exchange_weak(old = 0, __valubit, order, std::memory_order_relaxed)) {
+                __ticket.fetch_add(1, std::memory_order_relaxed);
+                return;
+            }
+            for (; old != 0 && i < 64; ++i, old = __atom.load(std::memory_order_relaxed))
+                details::__yield();
+        }
+        __lock_slow(order);
+    }
+
+    static constexpr uint32_t __vctm_thresh = 64;
+    static constexpr uint32_t __slack_range = 8;
+    static constexpr uint32_t __slack_value = 5;
+
+    static constexpr uint32_t __valubit = 1;
+    static constexpr uint32_t __vctmbit = 2;
+    static constexpr uint32_t __contbit = __slack_range * __vctmbit;
+    static constexpr uint32_t __vctmbits = (__contbit - 1) & ~__valubit;
+
+    static constexpr uint32_t __max_ticket = (std::numeric_limits<uint32_t>::max)();
+    static constexpr uint32_t __max_diff = __max_ticket >> 1;
+
+    __test_abi constexpr mutex() noexcept : __atom(0), __ticket(0), __tocket(__slack_value) {
+    }
+
+    mutex(const mutex&) = delete;
+    mutex &operator=(const mutex&) = delete;
+
+    __test_abi void __lock_slow(std::memory_order order) noexcept
+    {
+        bool ready = false, victim = false;
+
+        uint32_t old = __atom.fetch_add(__contbit, std::memory_order_relaxed) + __contbit;
+        uint32_t tick = __ticket.fetch_add(1, std::memory_order_relaxed);
+        uint32_t tock = __tocket.load(std::memory_order_relaxed);
+
+        details::__exponential_backoff b;
+        for(int i = 0;;++i) {
+            uint32_t diff = tock >= tick ? tock - tick : __max_ticket - tick + tock;
+            if(diff < __max_diff) {
+                if(!ready) { ready = true; b.reset(); }
+                if(diff > __vctm_thresh && !victim) { victim = true; old = __atom.fetch_add(__vctmbit, std::memory_order_relaxed) + __vctmbit; }
+                while((old & __valubit) == 0 && ((old & __vctmbits) == 0 || victim)) {
+                    uint32_t next = old - (victim ? __vctmbit : 0) - __contbit + __valubit;
+                    if (__atom.compare_exchange_weak(old, next, order, std::memory_order_relaxed))
+                        return;
+                }
+            }
+            if(i < 64)
+                details::__yield();
+            else
+                b.sleep();
+            tock = __tocket.load(std::memory_order_relaxed);
+            old = __atom.load(std::memory_order_relaxed);
+        }
+    }
+
+    atomic<uint32_t> __atom;
+    atomic<uint32_t> __ticket;
+    atomic<uint32_t> __tocket;
+};
+
+}}}
+
+
+*/
