dc32e50ec665ee848f2461ff93f84b0856a31a48
ogiroux <ogiroux@nvidia.com>
2017-07-31 20:16:14 -0700

    Removed the old and incorrect ARM tuning. Still working on the demo_mutex distillation.

--- test.hpp
+++ test.hpp
@@ -226,254 +226,82 @@ struct barrier {
     }
 };
 
-#ifdef __NVCC__
-
-namespace cuda { namespace experimental { inline namespace v1 { 
-
-namespace details {
-    __test_abi inline void __yield()
-    {
-#if !defined(__CUDA_ARCH__)
-        std::this_thread::yield();
-#elif defined(__has_cuda_nanosleep)
-        __mme_nanosleep(1);
-#endif
-    }
-    struct __exponential_backoff
-    {
-        unsigned time = 64;
-        __test_abi void reset() {
-            time = 64;
-        }
-        __test_abi void sleep()
-        {
-#if !defined(__CUDA_ARCH__)
-            std::this_thread::sleep_for(std::chrono::nanoseconds(time));
-#elif defined(__has_cuda_nanosleep)
-            __mme_nanosleep(time);
-#endif
-            time += 64 + (time >> 2);
-            if (time > 128*64) 
-                time = 128*64;
-        }
-    };
-}
-
-struct mutex
+struct demo_mutex
 {
-    __test_abi inline void unlock() noexcept {
-        if (__stolen.load(std::memory_order_relaxed))
-            __stolen.store(false, std::memory_order_relaxed);
+    __semaphore_abi inline void unlock() noexcept
+    {
+        if(stolen)
+            stolen = false;
         else
-            __tocket.store(__tocket.load(std::memory_order_relaxed) + 1, std::memory_order_relaxed);
-        __atom.fetch_and(~__valubit, std::memory_order_release);
+            tocket.fetch_add(1, std::memory_order_relaxed);
+        atom.fetch_and(~__valubit, std::memory_order_release);
     }
 
-    __test_abi inline void lock() noexcept {
-        uint32_t old = 0;
-        if (__builtin_expect(__atom.compare_exchange_weak(old = 0, __valubit, std::memory_order_acquire, std::memory_order_relaxed), 1)) {
-            __stolen.store(true, std::memory_order_relaxed);
+    __semaphore_abi inline void lock() noexcept
+    {
+        if (__semaphore_expect(try_lock(), 1))
             return;
-        }
-        uint32_t const tick = __ticket.fetch_add(1, std::memory_order_relaxed);
-        for (int i = 0; old == 0 && i < 32; ++i) {
-            if (__atom.compare_exchange_weak(old, __valubit, std::memory_order_acquire, std::memory_order_relaxed))
-                return;
-            for (; old != 0 && i < 32; ++i, details::__yield())
-                old = __atom.load(std::memory_order_relaxed);
-        }
-        __lock_slow(tick);
+        __lock_slow();
     }
-
-    static constexpr uint32_t __valubit = 1;
-    static constexpr uint32_t __contbit = 2;
-
-    __test_abi constexpr mutex() noexcept : __atom(0), __ticket(0), __tocket(0), __stolen(false) {
-    }
-
-    mutex(const mutex&) = delete;
-    mutex &operator=(const mutex&) = delete;
-
-    __test_abi void __lock_slow(uint32_t const tick) noexcept
+    __semaphore_abi inline bool try_lock() noexcept
     {
-        uint32_t contbit = 0;
-        for (int i = 0;; ++i) {
-            uint32_t old = __atom.load(std::memory_order_relaxed);
-            uint32_t const tock = __tocket.load(std::memory_order_relaxed);
-            uint32_t const pos_balance = tock > tick ? tock - tick : 0;
-            if (tock == tick || pos_balance) {
-                if (!contbit)
-                    if (pos_balance > 16 || __stolen.load(std::memory_order_relaxed))
-                        old = __atom.fetch_add(contbit = __contbit, std::memory_order_relaxed) + __contbit;
-                while (!(old & __valubit)) {
-                    if ((old & ~__valubit) && !contbit)
-                        break;
-                    uint32_t const next = old - contbit + __valubit;
-                    if (__atom.compare_exchange_weak(old, next, std::memory_order_acquire, std::memory_order_relaxed))
-                        return;
-                }
-            }
-            uint32_t const neg_balance = tock < tick ? tick - tock : 0;
-#if !defined(__CUDA_ARCH__)
-            std::this_thread::sleep_for(std::chrono::nanoseconds(neg_balance * 256 + 256));
-#elif defined(__has_cuda_nanosleep)
-            details::__mme_nanosleep(neg_balance * 256 + 256);
-#endif
-        }
-    }
-
-    atomic<uint32_t> __atom;
-    atomic<uint32_t> __ticket;
-    atomic<uint32_t> __tocket;
-    atomic<bool>     __stolen;
-};
-
-/*
-struct mutex
-{
-    __test_abi inline void unlock() noexcept {
-        __tocket.fetch_add(1, std::memory_order_relaxed);
-        __atom.fetch_and(~__valubit, std::memory_order_release);
-    }
-
-    __test_abi inline void lock() noexcept {
-        uint32_t const tick = __ticket.fetch_add(1, std::memory_order_relaxed);
         uint32_t old = 0;
-        for (int i = 0; i < 64; ++i) {
-            if(__atom.compare_exchange_weak(old = 0, __valubit, std::memory_order_acquire, std::memory_order_relaxed))
-                return;
-            for (; old != 0 && i < 64; ++i, old = __atom.load(std::memory_order_relaxed))
-                details::__yield();
+        for (int i = 0;i < 64; ++i) {
+            if(__semaphore_expect(atom.compare_exchange_strong(old = 0, __valubit, std::memory_order_acquire, std::memory_order_relaxed),1))
+                return stolen = true;
+            for(; old != 0 && i < 64; ++i) {
+#if !defined(__CUDA_ARCH__)
+                std::this_thread::yield();
+#elif defined(__has_cuda_nanosleep)
+                cuda::experimental::details::__mme_nanosleep(1);
+#endif
+                old = atom.load(std::memory_order_relaxed);
+            }
         }
-        __lock_slow(tick);
+        return false;
     }
 
+    __semaphore_abi constexpr demo_mutex() noexcept : atom(0), ticket(0), tocket(0), stolen(false)
+    {
+    }
+
+    demo_mutex(const demo_mutex&) = delete;
+    demo_mutex &operator=(const demo_mutex&) = delete;
+
+private:
     static constexpr uint32_t __valubit = 1;
     static constexpr uint32_t __contbit = 2;
 
-    __test_abi constexpr mutex() noexcept : __atom(0), __ticket(0), __tocket(0) {
-    }
-
-    mutex(const mutex&) = delete;
-    mutex &operator=(const mutex&) = delete;
-
-    __test_abi void __lock_slow(uint32_t const tick) noexcept
+    __semaphore_abi void __lock_slow() noexcept
     {
-        uint32_t contbit = 0;
-        for(int i = 0;;++i) {
-            uint32_t old = __atom.load(std::memory_order_relaxed);
-            uint32_t const tock = __tocket.load(std::memory_order_relaxed);
-            uint32_t const pos_balance = tock > tick ? tock - tick : 0;
-            uint32_t const neg_balance = tock < tick ? tick - tock : 0;
-            if(tock == tick || pos_balance) {
-                if(pos_balance > 32 && !contbit)
-                    old = __atom.fetch_add(contbit = __contbit, std::memory_order_relaxed) + __contbit;
-                while ((old & __valubit) == 0) {
-                    if (contbit == 0 && (old & ~__valubit))
-                        break;
-                    uint32_t const next = old - contbit + __valubit;
-                    if (__atom.compare_exchange_weak(old, next, std::memory_order_acquire, std::memory_order_relaxed))
-                        return;
-                }
-            }
+        uint32_t const tick = ticket.fetch_add(1, std::memory_order_relaxed);
+        uint32_t tock = tocket.load(std::memory_order_relaxed);
+        uint32_t contbit = 0u;
+        while(1) {
+            uint32_t const delta = (tick - tock) * 128;
 #if !defined(__CUDA_ARCH__)
-            std::this_thread::sleep_for(std::chrono::nanoseconds(neg_balance * 128 + 64));
+            std::this_thread::sleep_for(std::chrono::nanoseconds(delta));
 #elif defined(__has_cuda_nanosleep)
-            details::__mme_nanosleep(neg_balance * 128 + 64);
+            cuda::experimental::details::__mme_nanosleep(delta);
 #endif
+            tock = tocket.load(std::memory_order_relaxed);
+            if(tock != tick)
+                continue;
+            uint32_t old = atom.load(std::memory_order_relaxed);
+            while ((old & __valubit) == 0) {
+                uint32_t next = old - contbit + __valubit;
+                if (atom.compare_exchange_weak(old, next, std::memory_order_acquire, std::memory_order_relaxed))
+                    return;
+            }
+            if(contbit == 0)
+                atom.fetch_add(contbit = __contbit, std::memory_order_relaxed);
         }
     }
 
-    atomic<uint32_t> __atom;
-    atomic<uint32_t> __ticket;
-    atomic<uint32_t> __tocket;
+    atomic<uint32_t> atom;
+    atomic<uint32_t> ticket;
+    atomic<uint32_t> tocket;
+    bool stolen;
 };
-
-*/
-
-}}}
-
-#endif //__NVCC__
 
 #endif //TEST_HPP
-
-
-/*
-
-
-struct mutex
-{
-    __test_abi inline void unlock() noexcept {
-        __tocket.fetch_add(1, std::memory_order_relaxed);
-        __atom.fetch_and(~__valubit, std::memory_order_release);
-    }
-
-    __test_abi inline void lock(std::memory_order order = std::memory_order_seq_cst) noexcept {
-        uint32_t old = 0;
-        for (int i = 0; i < 64; ++i) {
-            if(__atom.compare_exchange_weak(old = 0, __valubit, order, std::memory_order_relaxed)) {
-                __ticket.fetch_add(1, std::memory_order_relaxed);
-                return;
-            }
-            for (; old != 0 && i < 64; ++i, old = __atom.load(std::memory_order_relaxed))
-                details::__yield();
-        }
-        __lock_slow(order);
-    }
-
-    static constexpr uint32_t __vctm_thresh = 64;
-    static constexpr uint32_t __slack_range = 8;
-    static constexpr uint32_t __slack_value = 5;
-
-    static constexpr uint32_t __valubit = 1;
-    static constexpr uint32_t __vctmbit = 2;
-    static constexpr uint32_t __contbit = __slack_range * __vctmbit;
-    static constexpr uint32_t __vctmbits = (__contbit - 1) & ~__valubit;
-
-    static constexpr uint32_t __max_ticket = (std::numeric_limits<uint32_t>::max)();
-    static constexpr uint32_t __max_diff = __max_ticket >> 1;
-
-    __test_abi constexpr mutex() noexcept : __atom(0), __ticket(0), __tocket(__slack_value) {
-    }
-
-    mutex(const mutex&) = delete;
-    mutex &operator=(const mutex&) = delete;
-
-    __test_abi void __lock_slow(std::memory_order order) noexcept
-    {
-        bool ready = false, victim = false;
-
-        uint32_t old = __atom.fetch_add(__contbit, std::memory_order_relaxed) + __contbit;
-        uint32_t tick = __ticket.fetch_add(1, std::memory_order_relaxed);
-        uint32_t tock = __tocket.load(std::memory_order_relaxed);
-
-        details::__exponential_backoff b;
-        for(int i = 0;;++i) {
-            uint32_t diff = tock >= tick ? tock - tick : __max_ticket - tick + tock;
-            if(diff < __max_diff) {
-                if(!ready) { ready = true; b.reset(); }
-                if(diff > __vctm_thresh && !victim) { victim = true; old = __atom.fetch_add(__vctmbit, std::memory_order_relaxed) + __vctmbit; }
-                while((old & __valubit) == 0 && ((old & __vctmbits) == 0 || victim)) {
-                    uint32_t next = old - (victim ? __vctmbit : 0) - __contbit + __valubit;
-                    if (__atom.compare_exchange_weak(old, next, order, std::memory_order_relaxed))
-                        return;
-                }
-            }
-            if(i < 64)
-                details::__yield();
-            else
-                b.sleep();
-            tock = __tocket.load(std::memory_order_relaxed);
-            old = __atom.load(std::memory_order_relaxed);
-        }
-    }
-
-    atomic<uint32_t> __atom;
-    atomic<uint32_t> __ticket;
-    atomic<uint32_t> __tocket;
-};
-
-}}}
-
-
-*/
